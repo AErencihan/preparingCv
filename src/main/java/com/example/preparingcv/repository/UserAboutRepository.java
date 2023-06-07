b7d091759a8dae82fb962142809c3cc2a6344d89
AErencihan <106157023+AErencihan@users.noreply.github.com>
2023-06-07 15:09:29 +0300

    added

---
+++ src/main/java/com/example/preparingcv/repository/UserAboutRepository.java
@@ -0,0 +1,8 @@
+package com.example.preparingcv.repository;
+
+import com.example.preparingcv.model.UserAbout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAboutRepository extends JpaRepository<UserAbout, Long> {
+
+}
