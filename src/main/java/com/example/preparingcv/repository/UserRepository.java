f06083ca546354c20815a72ef08145d429961b3e
AErencihan <106157023+AErencihan@users.noreply.github.com>
2023-06-08 22:55:30 +0300

    added

--- src/main/java/com/example/preparingcv/repository/UserRepository.java
+++ src/main/java/com/example/preparingcv/repository/UserRepository.java
@@ -3,6 +3,12 @@ package com.example.preparingcv.repository;
 import com.example.preparingcv.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String name);
+
+    Optional<User> findById(Long id);
 
 }
--- src/main/java/com/example/preparingcv/service/UserService.java
+++ src/main/java/com/example/preparingcv/service/UserService.java
@@ -16,12 +16,28 @@ public class UserService {
 
     public UserDto createUser(User user) {
         User saveUser = userRepository.save(user);
-
         return new UserDto.Builder()
                 .eMail(saveUser.geteMail())
                 .name(saveUser.getUserName())
                 .surname(saveUser.getUserName())
                 .build();
+    }
+
+    public User findUserByName(String name) {
+        return userRepository.findByUserName(name).orElseThrow();
+    }
+
+    public User updateUser(Long id, User user) throws Exception {
+        User updateUser = userRepository.findById(id)
+                .orElseThrow(()-> new Exception("not found"));
+
+        updateUser.setUserName(user.getUserName());
+        updateUser.setUserSurname(user.getUserSurname());
+        updateUser.setUserAbout(user.getUserAbout());
+
+        return userRepository.save(updateUser);
+
+
     }
 
 
